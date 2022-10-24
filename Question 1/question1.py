240859b6eb82d7f670c0baefe527c28a850939f7
Paige Faulkner <113376933+paigefaulkner@users.noreply.github.com>
2022-10-24 12:15:46 -0600

    Update question1.py

--- Question 1/question1.py
+++ Question 1/question1.py
@@ -1,3 +1,4 @@
 print(3405 + 2012 + 3104 + 1023)
 
-#I am printing the sum of the 4 numbers. To ensure I did not make a mistake, I compare the number in the command window with the number I get when using my calculator. 
+#I am printing the sum of the 4 numbers. To ensure I did not make a mistake, I compare the number in the command window with the number I get when using my calculator.
+#The output in the command prompt (9544) is correct. 
