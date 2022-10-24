122913c1da394a769e60eba8729e634926392e68
Paige Faulkner <113376933+paigefaulkner@users.noreply.github.com>
2022-10-24 12:15:06 -0600

    Create question1.py

---
+++ Question 1/question1.py
@@ -0,0 +1,3 @@
+print(3405 + 2012 + 3104 + 1023)
+
+#I am printing the sum of the 4 numbers. To ensure I did not make a mistake, I compare the number in the command window with the number I get when using my calculator. 
